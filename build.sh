3ef79cbb3981e9372c426b66f46f80c326143705
Nihil-Yet <rdbel_ozerov@outlook.com>
2026-02-19 00:46:34 +0300

    Added end msg to build.sh

--- build.sh
+++ build.sh
@@ -10,4 +10,6 @@ if [ ! -d build ]; then
 fi
 
 cd build
-g++ -o "${BIN_NAME}" ../main.cpp
+g++ -o "${BIN_NAME}" ../main.cpp && \
+    printf "\x1b[32mDone!\x1b[0m\n" || \
+    printf "\x1b[31mBuild failed!\x1b[0m\n"
