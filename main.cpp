b1d6a254f5cea790ee61e7ff3bde88f3d2124bd4
Nihil-Yet <rdbel_ozerov@outlook.com>
2026-02-19 12:40:31 +0300

    Use references instead of pointers
    - Use references in read_input function
    - Add print_output function

--- main.cpp
+++ main.cpp
@@ -28,33 +28,41 @@ struct InputData {
     std::vector<Figure> figures;
 };
 
-void read_input(InputData* data);
+void read_input(InputData& data);
+void print_output(char x, int y, int ping);
 
 int main()
 {
     InputData idata;
     while (1) {
-        read_input(&idata);
+        read_input(idata);
         if (idata.pong < 0) break;
     }
     return 0;
 }
 
-void read_input(InputData* data)
+void read_input(InputData& data)
 {
     int n;
-    std::cin >> data->pong >> n;
+    std::cin >> data.pong >> n;
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     
-    data->figures.resize(n);
+    data.figures.resize(n);
     std::string line;
     for (int i = 0; i < n; i++) {
         std::getline(std::cin, line); 
-        data->figures[i].position.x = line[0];
-        data->figures[i].position.y = line[1] - '0';
-        data->figures[i].color      = line[3];
-        data->figures[i].type       = static_cast<FigureType>(line[5] - '0');
+        data.figures[i].position.x = line[0];
+        data.figures[i].position.y = line[1] - '0';
+        data.figures[i].color      = line[3];
+        data.figures[i].type       = static_cast<FigureType>(line[5] - '0');
     }
 
     return;
 }
+
+void print_output(char x, int y, int ping, const std::string& msg)
+{
+    std::cout << "// " << msg << '\n'
+        << x << y << ' ' << ping; 
+}
+
